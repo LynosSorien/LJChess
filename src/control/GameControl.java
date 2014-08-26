49cbb5c982a14d6362ac5c309254294ac0b40930
xinsen <juanma_1267@hotmail.com>
2014-08-26 12:35:07 +0200

    Added getBoard method and the constructor for the clases dataControl and Control. References to issue #7

--- src/control/GameControl.java
+++ src/control/GameControl.java
@@ -1,7 +1,23 @@
 package control;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by david on 26/08/14.
  */
 public class GameControl extends Thread{
+    private Board board;
+    private List white;
+    private List black;
+
+    public GameControl(){
+        this.board = new Board();
+        this.white = new ArrayList<Piece>();
+        this.black = new ArrayList<Piece>();
+    }
+
+    public Board getBoard(){
+        return this.board;
+    }
 }
