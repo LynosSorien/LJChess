5d9dbd54266aa4f10f5241def80679ae9eeeed2a
xinsen <juanma_1267@hotmail.com>
2014-08-27 09:19:04 +0200

    Changed the atributes from GameControl to DataControl. References to issue #7

--- src/control/GameControl.java
+++ src/control/GameControl.java
@@ -2,28 +2,22 @@ package control;
 
 import java.util.ArrayList;
 import java.util.List;
+import model.Piece;
+import model.Board;
 
 /**
  * Created by david on 26/08/14.
  */
 public class GameControl extends Thread{
-    private Board board;
-    private List white;
-    private List black;
+
     private enum VictoryConditions {WIN,LOSE,DRAW,NONE}
 
     public GameControl(){
-        this.board = new Board();
-        this.white = new ArrayList<Piece>();
-        this.black = new ArrayList<Piece>();
+
     }
 
     @Override
     public void run(){
 
-    }
-
-    public Board getBoard(){
-        return this.board;
     }
 }
