083df330d116b6f95b2bfd16720a2a825475b674
David Jorquera <djorquab8@alumnes.ub.edu>
2014-08-29 10:13:29 +0200

    Update LJChessInstance.java

--- src/view/LJChessInstance.java
+++ src/view/LJChessInstance.java
@@ -1,1 +1,24 @@
 package view;
+
+public final class LJChessInstance {
+  private static Object lock = new Object();
+  private static LJChessInstance instance;
+  private IMain main_listener;
+  
+  private LJChessInstance() {}
+  
+  public static LJChessInstance instance() {
+    if (instance == null) {
+      synchronized (lock) {
+        if (instance == null) {
+          instance = new LJChessInstance();
+        }
+      }
+    }
+    return instance;
+  }
+  
+  public void addListener(Object listener) {
+    if (listener instanceof IMain) this.main_listener = listener;
+  }
+}
